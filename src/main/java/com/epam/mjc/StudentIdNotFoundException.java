2b3550a9e1ad51c73bfbb176130a732b86bcbaa0
Shodmon-creator <mamayusupovshodmon2003@gmail.com>
2022-11-29 11:22:45 +0500

    StudentIdNotFoundException has been created and task has been done

---
+++ src/main/java/com/epam/mjc/StudentIdNotFoundException.java
@@ -0,0 +1,8 @@
+package com.epam.mjc;
+
+public class StudentIdNotFoundException extends IllegalArgumentException {
+
+ public StudentIdNotFoundException(String message){
+     super(message);
+ }
+}
--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,16 +5,25 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID)throws StudentIdNotFoundException  {
+    if (Student.getValueOf(studentID)!=null) {
+      return Student.getValueOf(studentID);
+    }else {
+      throw new StudentIdNotFoundException("Could not find student with ID "+studentID);}
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+    for (long id : IDs) {
+      try {
+          Student student = manager.find(id);
+          System.out.println("Student name " + student.getName());
+      } catch (StudentIdNotFoundException e) {
+        System.err.println(e);
+      }
+
+
     }
 
   }
